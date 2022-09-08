02f7621af3a09fa72340e0159308b7a66bcc6a4f
steve <austines811@gmail.com>
2022-09-08 16:10:48 +0300

    adds attribute macros for relationships

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,4 @@
 class Owner < ActiveRecord::Base
     has_many :listings
+    has_many :users, through: :listings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+    belongs_to :owner
+    belongs_to :listing
 end
